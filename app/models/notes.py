965af9ce4ea78e13a54bbb9561a666f71d088adc
Conraad Strydom <conraadstrydom@gmail.com>
2026-03-25 20:15:01 +0200

    Add hashed password column to users

--- app/models/notes.py
+++ app/models/notes.py
@@ -63,6 +63,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, email={self.email!r})"
