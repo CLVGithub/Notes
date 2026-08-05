5fdaad06c1181eea0bf91855b474e4dcb0fd199e
conraadstrydom <conraadstrydom@gmail.com>
2026-08-05 19:20:51 +0200

    Add docker compose file for manual EC2 deployment

--- terraform/notes-module/networking.tf
+++ terraform/notes-module/networking.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.16.0.0/16"
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -8,7 +8,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.16.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 }
@@ -17,13 +17,13 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_route" "internet" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
 }
